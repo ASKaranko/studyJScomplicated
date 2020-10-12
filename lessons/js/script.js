8f23a43614281f6d3c24dabf9598652ec351e022
Karanko Andrei <andr.karanko@gmail.com>
2020-10-12 23:40:45 +0300

    Усложненное задание по уроку номер 3 Динамическая типизация

--- lessons/js/script.js
+++ lessons/js/script.js
@@ -1,12 +1,46 @@
-let num = 266219,
-  mult = 1;
+'use strict';
+//Дни недели
 
-let digitStr = num.toString();
+let daysOfWeekRu = ['Понедельник', 'Вторник', 'Среда', 'Черверг', 'Пятница', 'Суббота', 'Воскресенье'],
+  daysOfWeekEn = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
-for (let i=0; i < digitStr.length; i++) {
-  mult *= parseInt(digitStr[i], 10);
+let question = prompt('Выберите на каком языке отображать дни недели', 'ru, en');
+
+while(true) {
+  if (question === 'ru') {
+    console.log('Дни недели', daysOfWeekRu);
+    break;
+  } else if(question === 'en') {
+    console.log('Days of Week', daysOfWeekEn);
+    break;
+  } else {
+    question = prompt('Выберите правильный вариант: введите слово ru или en', 'ru, en');
+  }
 }
 
-console.log(mult);
-mult = mult ** 3;
-console.log(mult.toString().substring(0,2));
+switch (question) {
+  case 'ru':
+    console.log('Дни недели', daysOfWeekRu);
+    break;
+  case 'en':
+    console.log('Days of Week', daysOfWeekEn);
+    break;
+}
+
+// let objLang = {
+//   ru: daysOfWeekRu,
+//   en: daysOfWeekEn,
+// };
+
+// console.log(objLang.ru);
+
+let arrLang = [daysOfWeekRu, daysOfWeekEn];
+console.log(arrLang[0]);
+console.log(arrLang[1]);
+
+//Второе задание
+
+let namePerson = prompt('Выберите имя пользователя из списка или укажите другое имя', 'Артем, Максим');
+
+let result = namePerson === 'Артем' ? 'Директор' : namePerson === 'Максим' ? 'Преподаватель' : 'Студент';
+console.log(result);
