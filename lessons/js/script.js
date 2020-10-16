5d4b17956d6340e3df7969607f1729f26c371e25
Karanko Andrei <andr.karanko@gmail.com>
2020-10-17 00:31:16 +0300

    Усложненное домашнее задание по Уроку 7 Объекты и массивы

--- lessons/js/script.js
+++ lessons/js/script.js
@@ -1,20 +1,24 @@
 'use strict';
 
-//Задание 1
+let week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+let today = new Date;
 
-let arr = ['10000', '34568', '11204.21', '400501', '289283', '23232', '73256120'];
+console.log(today.getDay());
 
-for (let i = 0; i < arr.length; i++) {
-  let n = parseInt(arr[i].substring(0,1));
-  if (n === 2 || n === 4) {
-    console.log(parseInt(arr[i]));
+week.forEach(function (item, i, arr) {
+  if (i === (today.getDay()) && (i >= 5)) {
+    document.getElementById('day'+i).style.fontWeight='bold';
+    document.getElementById('day'+i).style.fontStyle='italic';
+    document.getElementById('day'+i).innerHTML=item;
+  } else if (i === (today.getDay())) {
+    document.getElementById('day'+i).style.fontWeight='bold';
+    document.getElementById('day'+i).innerHTML=item;
+  } else if (i >= 5) {
+    document.getElementById('day'+i).style.fontStyle='italic';
+    document.getElementById('day'+i).innerHTML=item;
+  } else {
+    document.getElementById('day'+i).innerHTML=item;
   }
-}
+});
 
-// Задание 2
 
-let arrSimple = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
-console.log("Простые числа до 100 в столбец");
-for (let i = 0; i < arrSimple.length; i++) {
-  console.log(arrSimple[i] + " Делитель данного числа: 1 и " + arrSimple[i]);
-}
