5a8ade59a56cebd052f26a9d2ec46ce55347c00a
Karanko Andrei <andr.karanko@gmail.com>
2020-10-20 19:22:50 +0300

    Усложенное домашнее задание по уроку номер 9 Доступ к элементам на странице

--- lessons/js/script.js
+++ lessons/js/script.js
@@ -1,24 +1,57 @@
 'use strict';
 
-let week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-let today = new Date;
+const dateVarA = document.querySelector('.date-a'),
+      dateVarB = document.querySelector('.date-b');
 
-console.log(today.getDay());
+let 
+    week = ['Восресенье', 'Понедельник', 'Вторник', 'Среда',      'Чертверг', 'Пятница', 'Суббота'],
+    month = ['Января', 'Февраля', 'Марта', 'Апреля', 'Мая', 'Июня', 'Июля', 'Августа', 'Сентрября', 'Октября', 'Ноября', 'Декабря'],
+    dayOfTheWeek = '',
+    monthOfTheYear = '',
+    declensionHour = '';
 
-week.forEach(function (item, i, arr) {
-  if (i === (today.getDay()) && (i === 6 || i === 0)) {
-    document.getElementById('day'+i).style.fontWeight='bold';
-    document.getElementById('day'+i).style.fontStyle='italic';
-    document.getElementById('day'+i).innerHTML=item;
-  } else if (i === (today.getDay())) {
-    document.getElementById('day'+i).style.fontWeight='bold';
-    document.getElementById('day'+i).innerHTML=item;
-  } else if (i === 6 || i === 0) {
-    document.getElementById('day'+i).style.fontStyle='italic';
-    document.getElementById('day'+i).innerHTML=item;
-  } else {
-    document.getElementById('day'+i).innerHTML=item;
+function addNull (elem) {
+  if (elem < 10) {
+    elem = '0' + elem;
+  } 
+  return elem;
+}
+
+function varA () {
+  let date = new Date ();
+
+  function declension () {
+    if (date.getHours() === 1 || date.getHours() === 21) {
+      declensionHour = 'час';
+    } else if ((date.getHours() >= 2 && date.getHours() <= 4) || date.getHours() === 22 || date.getHours() === 23) {
+      declensionHour = 'часа';
+    } else {
+      declensionHour = 'часов';
+    }
   }
-});
 
+  week.forEach(function (item, i, arr) {
+    if (i === date.getDay()) {
+      dayOfTheWeek = item;
+    }
+  });
+  
+  month.forEach(function (item, i, arr) {
+    if (i === date.getMonth()) {
+      monthOfTheYear = item;
+    }
+  });
 
+  declension();
+
+  dateVarA.innerHTML = `Сегодня ${dayOfTheWeek}, ${date.getDate()} ${monthOfTheYear} ${date.getFullYear()}, ${date.getHours()} ${declensionHour} ${date.getMinutes()} минут ${date.getSeconds()} секунды`;
+}
+
+function varB () {
+  let date = new Date ();
+
+  dateVarB.innerHTML = `${addNull(date.getDate())}.${addNull(date.getMonth()+1)}.${date.getFullYear()} - ${addNull(date.getHours())}:${addNull(date.getMinutes())}:${addNull(date.getSeconds())}`;
+}
+
+let timerA = setInterval(varA, 1000);
+let timerB = setInterval(varB, 1000);
