33aff2beaaa11d6a22d8ad8822a71b2aed3106fb
Karanko Andrei <andr.karanko@gmail.com>
2020-10-26 19:39:35 +0200

    Бонусное задание по уроку 13 Контекст вызова this

--- lessons/js/script.js
+++ lessons/js/script.js
@@ -1,57 +1,23 @@
 'use strict';
 
-const dateVarA = document.querySelector('.date-a'),
-      dateVarB = document.querySelector('.date-b');
+let bgColor = document.querySelector('.wrapper'),
+    h1 = document.querySelector('#color'),
+    btn = document.querySelector('#change');
 
-let 
-    week = ['Восресенье', 'Понедельник', 'Вторник', 'Среда',      'Чертверг', 'Пятница', 'Суббота'],
-    month = ['Января', 'Февраля', 'Марта', 'Апреля', 'Мая', 'Июня', 'Июля', 'Августа', 'Сентрября', 'Октября', 'Ноября', 'Декабря'],
-    dayOfTheWeek = '',
-    monthOfTheYear = '',
-    declensionHour = '';
+let getRandomColor = function () {
+  let letters = '0123456789ABCDEF',
+  color = '#';
 
-function addNull (elem) {
-  if (elem < 10) {
-    elem = '0' + elem;
-  } 
-  return elem;
-}
-
-function varA () {
-  let date = new Date ();
-
-  function declension () {
-    if (date.getHours() === 1 || date.getHours() === 21) {
-      declensionHour = 'час';
-    } else if ((date.getHours() >= 2 && date.getHours() <= 4) || date.getHours() === 22 || date.getHours() === 23) {
-      declensionHour = 'часа';
-    } else {
-      declensionHour = 'часов';
-    }
+  for (let i = 0; i < 6; i++) {
+    color +=  letters[Math.floor(Math.random() * 16)];
   }
+  return color;
+};
 
-  week.forEach(function (item, i, arr) {
-    if (i === date.getDay()) {
-      dayOfTheWeek = item;
-    }
-  });
+btn.addEventListener('click', function () {
+  let color = getRandomColor();
   
-  month.forEach(function (item, i, arr) {
-    if (i === date.getMonth()) {
-      monthOfTheYear = item;
-    }
-  });
-
-  declension();
-
-  dateVarA.innerHTML = `Сегодня ${dayOfTheWeek}, ${date.getDate()} ${monthOfTheYear} ${date.getFullYear()}, ${date.getHours()} ${declensionHour} ${date.getMinutes()} минут ${date.getSeconds()} секунды`;
-}
-
-function varB () {
-  let date = new Date ();
-
-  dateVarB.innerHTML = `${addNull(date.getDate())}.${addNull(date.getMonth()+1)}.${date.getFullYear()} - ${addNull(date.getHours())}:${addNull(date.getMinutes())}:${addNull(date.getSeconds())}`;
-}
-
-let timerA = setInterval(varA, 1000);
-let timerB = setInterval(varB, 1000);
+  bgColor.style.backgroundColor = `${color}`;
+  h1.innerHTML = `${color}`;
+  btn.style.color = `${color}`;
+});
