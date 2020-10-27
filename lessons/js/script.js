ce1a084e247dabeb61eb9b8f35d5d1eb9f76a8cc
Karanko Andrei <andr.karanko@gmail.com>
2020-10-28 01:01:11 +0200

    Усложненное домашнее задание по Уроку 14 ООП

--- lessons/js/script.js
+++ lessons/js/script.js
@@ -1,23 +1,58 @@
-'use strict';
+document.addEventListener('DOMContentLoaded', function () {
 
-let bgColor = document.querySelector('.wrapper'),
-    h1 = document.querySelector('#color'),
-    btn = document.querySelector('#change');
+  'use strict';
 
-let getRandomColor = function () {
-  let letters = '0123456789ABCDEF',
-  color = '#';
-
-  for (let i = 0; i < 6; i++) {
-    color +=  letters[Math.floor(Math.random() * 16)];
+  function DomElement (selector) {
+    this.selector = selector;
+    this.height = 100;
+    this.width = 100;
+    this.bg = 'grey';
+    this.fontSize = 14;
+    DomElement.prototype.createElem = function () {
+      if (this.selector[0] === '.') {
+        let div = document.createElement('div');
+        document.body.append(div);
+        div.classList.add(`${this.selector.substring(1)}`);
+        div.style.cssText = `height: ${this.height}px; width: ${this.width}px; background: ${this.bg}; font-size: ${this.fontSize}px; position: absolute;`;
+        div.textContent = 'Квадрат 100 на 100 px';
+      } else if (this.selector[0] === '#') {
+        let p = document.createElement('p');
+        document.body.append(p);
+        p.setAttribute('id', `${this.selector.substring(1)}`);
+        p.style.cssText = `height: ${this.height}px; width: ${this.width}px; background: ${this.bg}; font-size: ${this.fontSize}px;`;
+        p.textContent = 'Это созданный с помощью объекта класса DomElement элемент p';
+      }
+    }; 
+    DomElement.prototype.eventListeners = function () {
+      let div = document.querySelector('.square');
+      console.log(div);
+      document.addEventListener('keydown', function (event) {
+        console.log(event.code);
+        let computedStyle = getComputedStyle(div),
+            coord = 0;
+        if (event.code === 'ArrowLeft') {
+          coord = +computedStyle.left.replace('px', '');
+          div.style.left = `${coord - 10}px`;
+        } else if (event.code === 'ArrowRight') {
+          coord = +computedStyle.left.replace('px', '');
+          div.style.left = `${coord + 10}px`;
+        } else if (event.code === 'ArrowUp') {
+          coord = +computedStyle.top.replace('px', '');
+          div.style.top = `${coord - 10}px`;
+        } else if (event.code === 'ArrowDown') {
+          coord = +computedStyle.top.replace('px', '');
+          div.style.top = `${coord + 10}px`;
+        }
+      });
+    };
   }
-  return color;
-};
-
-btn.addEventListener('click', function () {
-  let color = getRandomColor();
   
-  bgColor.style.backgroundColor = `${color}`;
-  h1.innerHTML = `${color}`;
-  btn.style.color = `${color}`;
+  let objElement = new DomElement ('.square');
+  
+  console.log(objElement);
+  console.log(objElement instanceof DomElement);
+  
+  objElement.createElem();
+  objElement.eventListeners();
+
 });
